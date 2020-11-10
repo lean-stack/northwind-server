7af7f2cb70ac4a2240194d5f0a27975d6986a25f
Micha Alt <mail@lean-stack.de>
2020-11-10 21:56:08 +0100

    Adding an initial server with first data.

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
 #!/usr/bin/env node
 
+// Node modules
+const path = require('path');
+const jsonServer = require('json-server');
+
 console.log('Starting Northwind Server ...');
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, 'data', 'db.json'));
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(3000, () => {
+  console.log('JSON Server for Northwind data is running');
+});
